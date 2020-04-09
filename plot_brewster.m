fcaa74533ad3297334ec2a06485007bd61ad69e3
jean_paul_ <jean_paul_@users.noreply.github.com>
2020-04-09 17:35:42 +0200

    Laatste upload

--- plot_brewster.m
+++ plot_brewster.m
@@ -1,6 +1,10 @@
-function [minTheta,minFv] = plot_brewster(epsilon1,epsilon2)
+function [minTheta,minFv] = plot_brewster(epsilon1,epsilon2,plotflag)
 %PLOT_BREWSTER Plots the Fresnel reflection coefficients vs the incident
 %angle
+
+if (nargin <= 2)
+    plotflag = 'on';
+end
 
 N = 200;
 N = N+1;
@@ -17,39 +21,41 @@ end
 [minFv,mini] = min(abs(fresnel_v));
 minTheta = theta(mini);
 
-figure
-plot(theta,abs(fresnel_h))
-hold on
-plot(theta,abs(fresnel_v))
-xline(minTheta);
-yline(0);
-scatter(minTheta,minFv,'r','LineWidth',2)
-hold off
-xticks(0:pi/8:pi/2)
-xticklabels({'0','\pi/8','\pi/4','3\pi/8','\pi/2'})
-axis([0 pi/2 0 1])
-title('Magnitude of the Fresnel Reflection Coefficients')
-xlabel('\theta_i [rad]')
-ylabel('|\Gamma|')
-legend({'|\Gamma_H|','|\Gamma_V|'})
-text(minTheta+0.02,minFv+0.05,[sprintf('Brewster''s Angle =\n'),num2str(minTheta/pi),'\pi rad'])
-
-figure
-plot(theta,angle(fresnel_h))
-hold on
-plot(theta,angle(fresnel_v))
-xline(minTheta);
-yline(0);
-hold off
-xticks(0:pi/8:pi/2)
-yticks(-pi:pi/2:pi)
-xticklabels({'0','\pi/8','\pi/4','3\pi/8','\pi/2'})
-yticklabels({'-\pi','-\pi/2','0','\pi/2','\pi'})
-axis([0 pi/2 -pi pi])
-title('Phase of the Fresnel Reflection Coefficients')
-xlabel('\theta_i [rad]')
-ylabel('\angle\Gamma [rad]')
-legend({'\angle\Gamma_H','\angle\Gamma_V'})
+if (strcmp(plotflag,'on'))
+    figure
+    plot(theta,abs(fresnel_h))
+    hold on
+    plot(theta,abs(fresnel_v))
+    xline(minTheta);
+    yline(0);
+    scatter(minTheta,minFv,'r','LineWidth',2)
+    hold off
+    xticks(0:pi/8:pi/2)
+    xticklabels({'0','\pi/8','\pi/4','3\pi/8','\pi/2'})
+    axis([0 pi/2 0 1])
+    title('Magnitude of the Fresnel Reflection Coefficients')
+    xlabel('\theta_i [rad]')
+    ylabel('|\Gamma|')
+    legend({'|\Gamma_H|','|\Gamma_V|'})
+    text(minTheta+0.02,minFv+0.05,[sprintf('Brewster''s Angle =\n'),num2str(minTheta/pi),'\pi rad'])
+    
+    figure
+    plot(theta,angle(fresnel_h))
+    hold on
+    plot(theta,angle(fresnel_v))
+    xline(minTheta);
+    yline(0);
+    hold off
+    xticks(0:pi/8:pi/2)
+    yticks(-pi:pi/2:pi)
+    xticklabels({'0','\pi/8','\pi/4','3\pi/8','\pi/2'})
+    yticklabels({'-\pi','-\pi/2','0','\pi/2','\pi'})
+    axis([0 pi/2 -pi pi])
+    title('Phase of the Fresnel Reflection Coefficients')
+    xlabel('\theta_i [rad]')
+    ylabel('\angle\Gamma [rad]')
+    legend({'\angle\Gamma_H','\angle\Gamma_V'})
+end
 
 end
 
