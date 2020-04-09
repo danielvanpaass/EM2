7254c33c60baef1288ba37dfd9c2ff65310002f1
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2020-04-09 15:06:28 +0200

    del

--- fresnel_vs_incident_angle.m
+++
@@ -1,8 +0,0 @@
-epsilon_0 = 8.854e-12;
-epsilon_1 = 1;
-epsilon_2 = session2.task2.dielectric_prermittivity;
-theta_i = 0:0.01:90;
-
-[a, b] = Fresnel_reflection(epsilon_1,epsilon_2,theta_i);
-
-plot(theta_i,abs(b))
