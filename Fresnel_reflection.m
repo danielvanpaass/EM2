a86f34d584edb1b76058e25136e784fee682d1c3
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2020-04-09 14:18:24 +0200

    .

--- Fresnel_reflection.m
+++ Fresnel_reflection.m
@@ -1,5 +1,7 @@
 function [fresnel_h,fresnel_v] = Fresnel_reflection(epsilon1,epsilon2,theta_i)
 %input theta_i in rad
+%fres_v = parallel to incidence plane
+%fres_h = perpendicular to incidence plane
 fresnel_h = (cos(theta_i) - sqrt((epsilon2/epsilon1) - (sin(theta_i)).^2)) / (cos(theta_i) + sqrt((epsilon2/epsilon1) - (sin(theta_i)).^2));
 fresnel_v = -(epsilon2*cos(theta_i) - epsilon1*sqrt((epsilon2/epsilon1) - (sin(theta_i)).^2)) / (epsilon2*cos(theta_i) + epsilon1*sqrt((epsilon2/epsilon1) - (sin(theta_i)).^2));
 
